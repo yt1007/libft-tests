9043016df04892a7037469a766819aef917af365
Yea Ling Tay <yetay@u81z04s04.42kl.edu.my>
2023-05-09 15:10:12 +0800

    Cleaned up screen outputs

--- test/ft_memcpy/main.c
+++ test/ft_memcpy/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yetay <yetay@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 08:52:17 by yetay             #+#    #+#             */
-/*   Updated: 2023/05/05 14:05:29 by yetay            ###   ########.fr       */
+/*   Updated: 2023/05/09 15:09:47 by yetay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,14 @@ static int	memcpy_is_diff(char *dst, char *src, size_t n)
 	}
 	d[i] = 0;
 	e[i] = 0;
-	printf("Test: ft_memcpy(\"%s\", \"%s\", %lu)\n", dst, src, n);
 	if (memcpy(d, src, n) != ft_memcpy(d, src, n))
 	{
-		printf("----  FAILED.\n");
-		printf("dst: |%s|\n", dst);
-		printf("src: |%s|\n", src);
-		printf("memcpy(dst, src, %lu): |%s|\n", n, memcpy(d, src, n));
+		printf("   dst: |%s|\n", dst);
+		printf("   src: |%s|\n", src);
+		printf("   memcpy(dst, src, %lu): |%s|\n", n, memcpy(d, src, n));
 		printf("ft_memcpy(dst, src, %lu): |%s|\n\n", n, ft_memcpy(e, src, n));
 		return (1);
 	}
-	printf("----  PASSED!\n");
 	return (0);
 }
 
@@ -49,23 +46,26 @@ int	main(void)
 	char	dst[30];
 	char	*src;
 	int		i;
+	int		errors;
 
+	errors = 0;
 	i = 0;
 	while (i < 29)
 		dst[i++] = '_';
 	dst[i] = '\0';
 	src = "A string of character.";
 	if (memcpy_is_diff(dst, src, 0))
-		return (1);
+		errors++;
 	if (memcpy_is_diff(dst, src, 2))
-		return (1);
+		errors++;
 	if (memcpy_is_diff(dst, src, strlen(src)))
-		return (1);
+		errors++;
 	src = "";
 	if (memcpy_is_diff(dst, src, 0))
-		return (1);
+		errors++;
 	if (memcpy_is_diff(dst, src, 1))
-		return (1);
-	printf("All tests passed.\n");
+		errors++;
+	if (errors == 0)
+		printf("All tests passed.\n");
 	return (0);
 }
