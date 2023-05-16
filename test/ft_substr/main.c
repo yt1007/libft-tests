cfdee55a4b0493ae54edc9d22f49975c4eeb01f4
Yea Ling Tay <yetay@u81z05s06.42kl.edu.my>
2023-05-16 14:30:13 +0800

    Edited ft_substr.c: Added controls to handle cases where starting index is greater than the string length

--- test/ft_substr/main.c
+++ test/ft_substr/main.c
@@ -6,7 +6,7 @@
 /*   By: yetay <yetay@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 16:28:54 by yetay             #+#    #+#             */
-/*   Updated: 2023/05/09 17:15:45 by yetay            ###   ########.fr       */
+/*   Updated: 2023/05/16 14:24:00 by yetay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,8 @@ int	ft_substr_is_bad(char const *s, unsigned int start, size_t len, char *t)
 		printf("ft_substr: %s\n", substr);
 		return (1);
 	}
-	free(substr);
+	if (substr)
+		free(substr);
 	return (0);
 }
 
@@ -42,6 +43,7 @@ int	main(void)
 	errors = 0;
 	s = "A cat is not a dog.";
 	i = -1;
+	/* Intended use */
 	while (++i < ft_strlen(s))
 		if (ft_substr_is_bad(s, i, ft_strlen(s), s + i))
 			errors++;
@@ -51,16 +53,28 @@ int	main(void)
 		errors++;
 	if (ft_substr_is_bad(s, 0, ft_strlen(s), s))
 		errors++;
+	/* len exceptions */
 	if (ft_substr_is_bad(s, 0, ft_strlen(s) + 5, s))
 		errors++;
+	if (ft_substr_is_bad(s, 2, 0, ""))
+		errors++;
+	/* Empties */
 	if (ft_substr_is_bad("", 0, 1, ""))
 		errors++;
+	/* start index is end of string */
 	i = 0;
 	while (i < (ft_strlen(s) + 5))
 		if (ft_substr_is_bad(s, ft_strlen(s), i++, ""))
 			errors++;
-	if (ft_substr_is_bad(s, 2, 0, ""))
-		errors++;
+	i = -1;
+	while (++i <= 5)
+		if (ft_substr_is_bad(s, ft_strlen(s) + 1, i, ""))
+			errors++;
+	/* start index is beyond the string */
+	i = -1;
+	while (++i <= 5)
+		if (ft_substr_is_bad(s, ft_strlen(s) + 2, 0, ""))
+			errors++;
 	if (errors == 0)
 		printf("All tests passed.\n");
 	return (0);
