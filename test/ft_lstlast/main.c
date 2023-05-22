ca3161b4dea968d4f4a8965ed014531f6e5ff8e3
Yea Ling Tay <yetay@u82z05s05.42kl.edu.my>
2023-05-23 07:24:39 +0800

    Edited tests for ft_lstlast: added tests for lists created via ft_lstadd_back

--- test/ft_lstlast/main.c
+++ test/ft_lstlast/main.c
@@ -6,66 +6,107 @@
 /*   By: yetay <yetay@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 16:43:33 by yetay             #+#    #+#             */
-/*   Updated: 2023/05/19 16:58:39 by yetay            ###   ########.fr       */
+/*   Updated: 2023/05/23 07:19:29 by yetay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <string.h>
+#include <stdlib.h>
 
-static int	ft_lstlast_isbad(int n)
+static void	ft_putlst(t_list *lst)
 {
-	t_list	**lst;
-	t_list	*new;
 	t_list	*pl;
+
+	pl = lst;
+	while (pl->next)
+	{
+		ft_putstr_fd(pl->content, 2);
+		ft_putstr_fd(" ", 2);
+		pl = pl->next;
+	}
+	ft_putendl_fd(pl->content, 2);
+}
+
+static void	ft_freelst(t_list *lst)
+{
+	t_list	*tmp;
+
+	while (lst->next)
+	{
+		tmp = lst;
+		lst = lst->next;
+		if (tmp)
+			free(tmp);
+	}
+	if (lst)
+		free(lst);
+}
+
+int	ft_lstlast_isbad(int n)
+{
+	t_list	**lf;
+	t_list	**lr;
+	t_list	*lf_head;
+	t_list	*lr_head;
+	t_list	*lf_last;
+	t_list	*lr_last;
 	int		i;
 
-	if (n == 0)
-	{
-		if (ft_lstlast(NULL) != 0)
-			return (1);
-		else
-			return (0);
-	}
+	lf = NULL;
+	lr = NULL;
 	i = -1;
-	lst = NULL;
 	while (++i < n)
 	{
-		if (lst)
-			ft_lstadd_front(lst, ft_lstnew(ft_itoa(i)));
+		if (lf)
+			ft_lstadd_front(lf, ft_lstnew(ft_itoa(i)));
 		else
 		{
-			new = ft_lstnew(ft_itoa(i));
-			lst = &new;
+			lf_head = ft_lstnew(ft_itoa(i));
+			lf = &lf_head;
+		}
+		if (lr)
+			ft_lstadd_back(lr, ft_lstnew(ft_itoa(i)));
+		else
+		{
+			lr_head = ft_lstnew(ft_itoa(i));
+			lr = &lr_head;
 		}
 	}
-	pl = ft_lstlast(*lst);
-	if (pl->next || strcmp(pl->content, "0"))
+	lf_last = ft_lstlast(*lf);
+	lr_last = ft_lstlast(*lr);
+	if (strcmp(lf_last->content, "0") || strcmp(lr_last->content, ft_itoa(n - 1)))
 	{
-		ft_putstr_fd("         n: ", 2);
+		ft_putstr_fd("Lists with ", 2);
 		ft_putnbr_fd(n, 2);
-		ft_putendl_fd("", 2);
-		ft_putstr_fd("ft_lstlast: ", 2);
-		ft_putstr_fd(pl->content, 2);
-		ft_putendl_fd("", 2);
-		ft_putstr_fd("Expected: ", 2);
-		ft_putstr_fd("0", 2);
-		ft_putendl_fd("", 2);
+		ft_putendl_fd(" elements:", 2);
+		ft_putlst(*lf);
+		ft_putlst(*lr);
+		if (*lf)
+			ft_freelst(*lf);
+		if (*lr)
+			ft_freelst(*lr);
 		return (1);
 	}
+	if (*lf)
+		ft_freelst(*lf);
+	if (*lr)
+		ft_freelst(*lr);
 	return (0);
 }
 
 int	main(void)
 {
 	int	errors;
-	int	n;
+	int	i;
 
 	errors = 0;
-	n = -1;
-	while (++n < 3)
-		if (ft_lstlast_isbad(n))
+	i = 0;
+	while (++i < 64)
+		if (ft_lstlast_isbad(i))
 			errors++;
+	if (ft_lstlast_isbad(1024))
+		errors++;
 	if (errors == 0)
 		ft_putendl_fd("All tests passed.", 2);
 	return (0);
